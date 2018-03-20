88bf1638c31faeb198bd486821d0b8fe83a92084
slstein14 <sstein.blhs@gmail.com>
2018-03-19 21:00:37 -0400

    Added Test input client

---
+++ RoboCopClient/mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QTcpSocket>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    QString hostIP;
+
+private slots:
+    void on_Connect_clicked();
+    void Disconnected();
+    void readyRead();
+
+    void on_Jump_clicked();
+
+    void on_Left_clicked();
+
+    void on_Stop_clicked();
+
+    void on_Right_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QTcpSocket *socket;
+};
+
+#endif // MAINWINDOW_H
