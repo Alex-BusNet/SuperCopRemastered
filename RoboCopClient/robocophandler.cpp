6a63263450a6b2d70548e457f97cde3d836549f5
Alex Portolese <dietodeath23@gmail.com>
2018-04-04 11:48:17 -0500

    Added main game loop function for NN evalutaion.

--- RoboCopClient/robocophandler.cpp
+++ RoboCopClient/robocophandler.cpp
@@ -4,3 +4,12 @@ RoboCopHandler::RoboCopHandler()
 {
 
 }
+
+void RoboCopHandler::GameLoop()
+{
+    while(true)
+    {
+//        Species s = pool->species[pool->currentSpecies];
+
+    }
+}
--- RoboCopClient/robocophandler.h
+++ RoboCopClient/robocophandler.h
@@ -3,13 +3,22 @@
 
 // Used to manage the NN / Genetic Algorithm
 #include "nnglobals.h"
+#include "gene.h"
+#include "genome.h"
+#include "pool.h"
+#include "neuron.h"
+#include "network.h"
+#include "species.h"
 
 class RoboCopHandler
 {
 public:
     RoboCopHandler();
 
+    void GameLoop();
 private:
+    Pool *pool;
+
 
 };
 
